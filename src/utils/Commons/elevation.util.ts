602a202afdb415f2b348b70df9d8e79ac5e3c3f1
Arnaud Prioul <arnaud.prioul@gmail.com>
2025-05-15 14:43:29 +0200

    [WIP] - Fix Elevation composable to calculate box shadow

--- src/utils/Commons/elevation.util.ts
+++ src/utils/Commons/elevation.util.ts
@@ -1,10 +1,10 @@
 import { convertToUnit, normalize, roundTo } from "@foxy/utils"
 
 export function formatElevationStyle (elevation: number = 0) {
-    const blurRadius = roundTo(normalize(elevation * .01, 0, 1, 0, 16), 1);
-    const offsetX = roundTo(normalize(elevation * .01, 0, 1, 0, 8), 1);
-    const offsetY = roundTo(normalize(elevation * .01, 0, 1, 0, 16), 1);
-    const opacity = roundTo(normalize(elevation * .01, 0, 1, 0.5, 0.25), 2);
+    const blurRadius = roundTo(normalize(elevation * .04, 0, 1, 0, 16), 1);
+    const offsetX = roundTo(normalize(elevation * .04, 0, 1, 0, 8), 1);
+    const offsetY = roundTo(normalize(elevation * .04, 0, 1, 0, 16), 1);
+    const opacity = roundTo(normalize(elevation * .04, 0, 1, 0.5, 0.25), 2);
 
     return `box-shadow: ${convertToUnit(offsetX)} ${convertToUnit(offsetY)} ${convertToUnit(blurRadius)} hsl(0deg 0% 0% / ${opacity.toFixed(2)})`
 }
