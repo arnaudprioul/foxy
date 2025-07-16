fb4e02d134b35e492e602abe9b78a1cb6c79190d
Arnaud Prioul <arnaud.prioul@gmail.com>
2025-07-16 21:18:33 +0200

    [WIP] - add test vite config

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+var resolve = function (file) { return path.resolve(__dirname, file); };
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: [
+            { find: /^foxy$/, replacement: resolve('./src/foxy.ts') },
+            { find: /^@foxy/, replacement: fileURLToPath(new URL("./src", import.meta.url)) },
+            { find: /^@stories/, replacement: fileURLToPath(new URL("./stories", import.meta.url)) },
+        ],
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    },
+});
