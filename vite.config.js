9e537ff900a6598ccb681ff7bc5b2563b8f42525
Arnaud Prioul <arnaud.prioul@gmail.com>
2025-07-21 22:21:51 +0200

    [WIP] - update version

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { fileURLToPath } from 'node:url';
 import path from 'path';
 import { defineConfig } from 'vite';
 import commonjs from "vite-plugin-commonjs";
+import tsconfigPaths from "vite-tsconfig-paths";
 var resolve = function (file) { return path.resolve(__dirname, file); };
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), commonjs()],
+    plugins: [vue(), commonjs(), tsconfigPaths()],
     resolve: {
         alias: [
             { find: /^origam$/, replacement: resolve('./src/origam.ts') },
